ec05cd4d8b696a430feb8f42d340a354586f0fac
xxwTeo <3141929416@qq.com>
2026-06-24 15:56:44 +0800

    Add files via upload
    6.23 学习IO流并编写相应代码巩固

---
+++ IOstream/src/test/java/com/xxw/iostream/IOstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxw.iostream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IOstreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
